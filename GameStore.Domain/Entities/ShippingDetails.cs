3cbb7db25afeba1b94a715610b38b32051d89faa
Виктор Дзюба <forusbanr91@gmail.com>
2019-02-28 23:03:09 +0300

    Initial commit

---
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Укажите как вас зовут")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите адрес доставки")]
+        [Display(Name = "Адрес:")]
+        public string Adress { get; set; }
+
+        [Required(ErrorMessage = "Укажите город")]
+        [Display(Name = "Город:")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Укажите страну")]
+        [Display(Name = "Страна:")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
